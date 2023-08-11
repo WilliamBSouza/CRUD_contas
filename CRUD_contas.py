d8d6c14336a90cff38608548a5dda1e581a70f0b
William Souza <williambarrosdesouza34@gmail.com>
2023-08-10 23:31:23 -0300

    Criado as tebelas

--- CRUD_contas.py
+++ CRUD_contas.py
@@ -5,4 +5,31 @@ import sqlite3
 conexao = sqlite3.connect('contas.db')
 cursor = conexao.cursor()
 
+#criando as tabelas
+"""
+cursor.execute('''
+    CREATE TABLE ContasAPagar (
+        id INTEGER PRIMARY KEY,
+        descricao TEXT NOT NULL,
+        valor REAL NOT NULL,
+        data_vencimento DATE NOT NULL,
+        pago BOOLEAN NOT NULL
+    )
+''')
+
+cursor.execute('''
+CREATE TABLE ContasAReceber (
+    id INTEGER PRIMARY KEY,
+    descricao TEXT NOT NULL,
+    valor REAL NOT NULL,
+    data_A_Receber DATE NOT NULL,
+    Recebido BOOLEAN NOT NULL
+);
+''')
+
+"""
+
+# Salvando as alterações e fechando a conexão
+conexao.commit()
+conexao.close()
 
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 Criar um banco de dados para armazenar as contas 
 Criar as tabelas para inserir as contas 
 Criar as mecanicas para inseir e mover as contas de uma tabela para outra 
+
+
+
+10-08-2023 - foi criado as tabelas para inserir as contas a pagar e contas a receber 
